33789beeb5d9c6979dfba2b105e95c94f828c900
masomj <93717696+masomj@users.noreply.github.com>
2022-05-23 14:57:13 +0100

    changed mileage set up, added python code

--- run.py
+++ run.py
@@ -13,36 +13,25 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    data =[]
-    with open("data/comp.json","r") as json_data:
-        data = json.load(json_data)
-    return render_template("about.html",page_title="About", company = data)
+@app.route("/cv20", methods=["GET","POST"])
+def cv20():    
+    
+    start_location = request.form.get("start-location")
+    end_location = request.form.get("end-location")
+    start_mileage = request.form.get("start-mileage")
+    end_mileage = request.form.get("end-mileage")
+    username = request.form.get("username")
+    start_time = request.form.get("start-time")
+    end_time = request.form.get("end_time")
+    print(end_time)
+    print(start_time)
+    return render_template("cv20.html")
+        
 
+@app.route("/cu18", methods=["GET","POST"])
+def cu18():
+    return render_template("cu18.html")
 
-@app.route("/contact", methods=["GET","POST"])
-def contact():
-    flash("Thanks, {} we have received your message!".format(
-        request.form.get("name")))
-    return render_template("contact.html",page_title="Contact")
-
-
-
-@app.route("/careers")
-def careers():
-    return render_template("careers.html",page_title="Careers")
-
-@app.route("/about/<member_name>")
-def about_member(member_name):
-    member =[]
-    with open ("data/comp.json","r") as json_data:
-        data =json.load(json_data)
-        for obj in data:
-            if obj["url"] == member_name:
-                member = obj
-                
-    return render_template("member.html",member=member)
 
 if __name__ == "__main__":
     app.run(
